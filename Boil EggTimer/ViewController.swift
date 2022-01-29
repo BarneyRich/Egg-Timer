6beeb1e1e3916c6da863d5cfe590bb3e975d41a2
Bala Barnabas Simon <barnabassimon486@gmail.com>
2022-01-29 14:40:33 +0300

    Egg Timer completed without alarm sound.

--- Boil EggTimer/ViewController.swift
+++ Boil EggTimer/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimesHardness = ["Soft": 6, "Medium": 9, "Hard": 12]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        progressBar.progress = 0.0
     }
-
-
+    
+    
+    
+    @IBAction func EggHardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimesHardness[hardness]!
+        
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLable.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        if(secondsPassed < totalTime) {
+            
+            secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = percentageProgress
+          print("\(secondsPassed) seconds")
+            
+            
+        } else {
+            timer.invalidate()
+            titleLable.text = "DONE"
+        }
+    }
+    
+    
+    
 }
-
